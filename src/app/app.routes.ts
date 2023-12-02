ff77d06d97b1f03a7b8914f69b4c01cb803b5711
HARSHITA PRIYA <harshitapriyaph5@gmail.com>
2023-12-03 04:10:36 +0530

    added routing facilities

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { CreateTaskComponent } from './create-task/create-task.component';
+import { UpdateTaskComponent } from './update-task/update-task.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: SignInComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'sign-in',
+    component: SignInComponent,
+  },
+  {
+    path: 'sign-up',
+    component: SignUpComponent,
+  },
+  {
+    path: 'create-task',
+    component: CreateTaskComponent,
+  },
+  {
+    path: 'update-task',
+    component: UpdateTaskComponent,
+  },
+];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { CreateTaskComponent } from '../create-task/create-task.component';
 import { UpdateTaskComponent } from '../update-task/update-task.component';
 import { TaskComponent } from '../task/task.component';
@@ -6,8 +7,23 @@ import { TaskComponent } from '../task/task.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CreateTaskComponent, UpdateTaskComponent, TaskComponent],
+  imports: [
+    CreateTaskComponent,
+    UpdateTaskComponent,
+    TaskComponent,
+    CommonModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  tasks = [
+    { title: 'Task 1', description: 'Description 1' },
+    { title: 'Task 2', description: 'Description 2' },
+    // add more tasks here
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
